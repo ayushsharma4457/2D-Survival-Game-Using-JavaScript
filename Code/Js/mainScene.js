dc37ebeb79db9d767d0fa12b34945c20be447574
Ayush Sharma <ayushsharma4457@gmail.com>
2024-12-13 11:50:13 +0530

    2D Survival Game Using JavaScript
    Today i was load the player skin and create player in create method, i can also add the animation walking animation with add some functionality using if else-if when the player moves then walk animation start and when player is stop then animation is idle start.

--- Code/Js/mainScene.js
+++ Code/Js/mainScene.js
@@ -6,11 +6,14 @@ export default class MainScene extends Phaser.Scene{
 
     preload(){
         console.log("Preload");
+        this.load.atlas('male','assets/images/male.png','assets/images/male_atlas.json');
+        this.load.animation('male_anim','assets/images/male_anim.json');
     }
 
     create(){
         console.log("Create");
-        this.player = new Phaser.Physics.Matter.Sprite(this.matter.world);
+        this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,0,0,'male','townsfolk_m_idle_1');
+        this.add.existing(this.player);
         this.inputKeys = this.input.keyboard.addKeys({
             up : Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -20,6 +23,7 @@ export default class MainScene extends Phaser.Scene{
     }
     update(){
         console.log("Update");
+        // this.player.anims.play('male_walk',true)
         const speed = 1.5;
         let playerVelocity = new Phaser.Math.Vector2();
         if(this.inputKeys.left.isDown){
@@ -37,5 +41,14 @@ export default class MainScene extends Phaser.Scene{
         playerVelocity.normalize();
         playerVelocity.scale(speed);
         this.player.setVelocity(playerVelocity.x,playerVelocity.y);
+
+        // This New Movement Function Is Added: 
+        if (playerVelocity.length() > 0) {
+            // Always play the walking animation when the player is moving
+            this.player.anims.play('male_walk', true);
+        }else if(playerVelocity.length()== 0 )
+        {
+            this.player.setFrame('male_idle');
+        }
     }
 }
