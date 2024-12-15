99eea5153619690946f39a22d1d661b1cc9754e6
Ayush Sharma <ayushsharma4457@gmail.com>
2024-12-15 16:47:49 +0530

    2D Survival Game Using JavaScript
    Today i was create a new JS file in which i store the data of player and encapsulate the complexity with which the main scene file look cleaner.

--- Code/Js/mainScene.js
+++ Code/Js/mainScene.js
@@ -1,3 +1,5 @@
+import player from "./player.js";
+
 export default class MainScene extends Phaser.Scene{
     constructor()
     {
@@ -6,49 +8,21 @@ export default class MainScene extends Phaser.Scene{
 
     preload(){
         console.log("Preload");
-        this.load.atlas('male','assets/images/male.png','assets/images/male_atlas.json');
-        this.load.animation('male_anim','assets/images/male_anim.json');
+        player.preload(this);
     }
 
     create(){
         console.log("Create");
-        this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,0,0,'male','townsfolk_m_idle_1');
-        this.add.existing(this.player);
-        this.inputKeys = this.input.keyboard.addKeys({
+        this.player = new player({scene:this,x:0,y:0,texture:'male',frame:'townsfolk_m_idle_1'});
+        this.player.inputKeys = this.input.keyboard.addKeys({
             up : Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D,
         })
     }
-    update(){
-        console.log("Update");
-        // this.player.anims.play('male_walk',true)
-        const speed = 1.5;
-        let playerVelocity = new Phaser.Math.Vector2();
-        if(this.inputKeys.left.isDown){
-            playerVelocity.x = -1;
-        }else if(this.inputKeys.right.isDown)
-        {
-            playerVelocity.x = 1;
-        }
-        if(this.inputKeys.up.isDown){
-            playerVelocity.y = -1;
-        }else if(this.inputKeys.down.isDown)
-        {
-            playerVelocity.y = 1;
-        }
-        playerVelocity.normalize();
-        playerVelocity.scale(speed);
-        this.player.setVelocity(playerVelocity.x,playerVelocity.y);
-
-        // This New Movement Function Is Added: 
-        if (playerVelocity.length() > 0) {
-            // Always play the walking animation when the player is moving
-            this.player.anims.play('male_walk', true);
-        }else if(playerVelocity.length()== 0 )
-        {
-            this.player.setFrame('male_idle');
-        }
-    }
+   update()
+   {
+    this.player.update();
+   }
 }
